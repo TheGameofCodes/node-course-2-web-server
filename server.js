ddf4b3a8638643d6ecbb3de43b662ee4f14f45cc
Ashraf <ashrafqureshi313@gmail.com>
2019-02-03 19:01:31 +0530

    Added portfolio

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/', (req,res) => {
     });
 });
 
+app.get('/portfolio', (req,res) => {
+    res.render('portfolio.hbs', {
+        pageTitle : 'Portfolio Page'
+    });
+});
+
 app.get('/about',(req,res) => {
     res.render('about.hbs',{
         pageTitle : 'About Page',
